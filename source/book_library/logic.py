a3404a0365de036e4f78b57abdafd455905df6c5
Renato Esposito <renato.esposito1999@outlook.com>
2024-06-24 18:30:45 +0200

    Added book's library

---
+++ source/book_library/logic.py
@@ -0,0 +1,101 @@
+"""
+Representations and Inference for Logic. (Chapters 7-9, 12)
+
+Covers both Propositional and First-Order Logic. First we have four
+important data types:
+
+    KB            Abstract class holds a knowledge base of logical expressions
+    KB_Agent      Abstract class subclasses agents.Agent
+    Expr          A logical expression, imported from utils.py
+    substitution  Implemented as a dictionary of var:value pairs, {x:1, y:x}
+
+Be careful: some functions take an Expr as argument, and some take a KB.
+
+Logical expressions can be created with Expr or expr, imported from utils, TODO
+or with expr, which adds the capability to write a string that uses
+the connectives ==>, <==, <=>, or <=/=>. But be careful: these have the
+operator precedence of commas; you may need to add parens to make precedence work.
+See logic.ipynb for examples.
+
+Then we implement various functions for doing logical inference:
+
+    pl_true          Evaluate a propositional logical sentence in a model
+    tt_entails       Say if a statement is entailed by a KB
+    pl_resolution    Do resolution on propositional sentences
+    dpll_satisfiable See if a propositional sentence is satisfiable
+    WalkSAT          Try to find a solution for a set of clauses
+
+And a few other functions:
+
+    to_cnf           Convert to conjunctive normal form
+    unify            Do unification of two FOL sentences
+    diff, simp       Symbolic differentiation and simplification
+"""
+
+import heapq
+import itertools
+import random
+from collections import defaultdict, Counter
+
+import networkx as nx
+import utils
+class KB:
+    """A knowledge base to which you can tell and ask sentences.
+    To create a KB, first subclass this class and implement
+    tell, ask_generator, and retract. Why ask_generator instead of ask?
+    The book is a bit vague on what ask means --
+    For a Propositional Logic KB, ask(P & Q) returns True or False, but for an
+    FOL KB, something like ask(Brother(x, y)) might return many substitutions
+    such as {x: Cain, y: Abel}, {x: Abel, y: Cain}, {x: George, y: Jeb}, etc.
+    So ask_generator generates these one at a time, and ask either returns the
+    first one or returns False."""
+
+    def __init__(self, sentence=None):
+        if sentence:
+            self.tell(sentence)
+
+    def tell(self, sentence):
+        """Add the sentence to the KB."""
+        raise NotImplementedError
+
+    def ask(self, query):
+        """Return a substitution that makes the query true, or, failing that, return False."""
+        return utils.first(self.ask_generator(query), default=False)
+
+    def ask_generator(self, query):
+        """Yield all the substitutions that make query true."""
+        raise NotImplementedError
+
+    def retract(self, sentence):
+        """Remove sentence from the KB."""
+        raise NotImplementedError
+
+
+class PropKB(KB):
+    """A KB for propositional logic. Inefficient, with no indexing."""
+
+    def __init__(self, sentence=None):
+        super().__init__(sentence)
+        self.clauses = []
+
+    def tell(self, sentence):
+        """Add the sentence's clauses to the KB."""
+        self.clauses.extend(utils.conjuncts(utils.to_cnf(sentence)))
+
+    def ask_generator(self, query):
+        """Yield the empty substitution {} if KB entails query; else no results."""
+        if utils.tt_entails(utils.Expr('&', *self.clauses), query):
+            yield {}
+
+    def ask_if_true(self, query):
+        """Return True if the KB entails query, else return False."""
+        for _ in self.ask_generator(query):
+            return True
+        return False
+
+    def retract(self, sentence):
+        """Remove the sentence's clauses from the KB."""
+        for c in utils.conjuncts(utils.to_cnf(sentence)):
+            if c in self.clauses:
+                self.clauses.remove(c)
+
--- source/minesweeper_logical.py
+++ source/minesweeper_logical.py
@@ -1,20 +1,27 @@
 from math import floor
 import settings as st
 import random
+from book_library import PropKB
+
+
+
 # LE FUNZIONI LE POSSO NOMINARE IN MANIERA TUTTE PICCOLE E CON _ PER SPERARE NOMI
 # Le variabili devono essere descrittive lower case e separate da _ 
 # I nomi dei moduli devono essere tutti piccoli e separati da _ 
 # le costanti upper case.
+# Logical rapresentation of the game.
 class LogicalMinesweeper:
     def __init__(self, ui_instance, safe_row,safe_col):
         self.ui = ui_instance
+        self.ai = PropKB()
         # Initialization of the logical field
         self.field = []
-        # field is a list of lists, each list is a row of the filed, each element of this rows 
+        # field is a list of lists, each list is a row of the filed, each element of this rows can be TRUE/numbers if it is safe
+        # false if contains a mine. 
         for row in range(st.N_ROWS):
             new_row = []
             for col in range(st.N_COLS):
-                new_row.append(0)
+                new_row.append(True)
             self.field.append(new_row)
          # LIST COMPRESSION --> field = [[0 for _ in range(self.N_COLS)] for _ in range(self.N_ROWS)]
         '''
@@ -27,37 +34,37 @@ class LogicalMinesweeper:
     [0, 0, 0, 0, 0, 0]   # Sesta riga
     ]
     '''
-
         # Posizionamento delle mine
         mines_placed = 0
         while mines_placed < st.N_MINES:
             row = safe_row
             col = safe_col
-            print(f"safe_row = {safe_row} safe_col{safe_col}")
+            print(f"safe_row = {safe_row} safe_col = {safe_col}")
             while row == safe_row:
                 row = random.randint(0, st.N_ROWS - 1)
             while col == safe_col:
                 col = random.randint(0, st.N_COLS - 1)
-            if self.field[row][col] != -1:
-                self.field[row][col] = -1
+            if self.field[row][col] != False:
+                self.field[row][col] = False
                 mines_placed += 1
 
+
+        # Stampa del campo (solo per scopi di debug)
+        for row in self.field:
+            print(" ".join(str(cell) if cell != False else "*" for cell in row))
+
+    def count_mines(self):
         # Calcolo del numero di mine adiacenti per ogni cella
         for row in range(st.N_ROWS):
             for col in range(st.N_COLS):
-                if self.field[row][col] == -1:
+                if self.field[row][col] == False:
                     continue
                 count = 0
                 for i in range(max(0, row - 1), min(st.N_ROWS, row + 2)):
                     for j in range(max(0, col - 1), min(st.N_COLS, col + 2)):
-                        if self.field[i][j] == -1:
+                        if self.field[i][j] == False:
                             count += 1
                 self.field[row][col] = count
-
-        # Stampa del campo (solo per scopi di debug)
-        for row in self.field:
-            print(" ".join(str(cell) if cell != -1 else "*" for cell in row))
-
 
         
         
